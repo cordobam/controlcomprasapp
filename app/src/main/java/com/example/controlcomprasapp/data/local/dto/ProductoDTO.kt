b7f735275fce2b9ac90326575f96274b1a962990
cordobam <cordobam1987@gmail.com>
2026-03-28 19:55:49 -0300

    Se hicieron cambios para  completar la pagina home en donde van a aparecer distintos indicadores

---
+++ app/src/main/java/com/example/controlcomprasapp/data/local/dto/ProductoDTO.kt
@@ -0,0 +1,6 @@
+package com.example.controlcomprasapp.data.local.dto
+
+data class ProductoDTO(
+    val cant_veces: Int,
+    val nombre: String
+)
--- app/src/main/java/com/example/controlcomprasapp/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/controlcomprasapp/viewmodel/HomeViewModel.kt
@@ -5,13 +5,44 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.controlcomprasapp.data.local.dto.DescuentosDTO
+import com.example.controlcomprasapp.data.local.dto.GastoMensualDTO
+import com.example.controlcomprasapp.data.local.dto.ItemTicketDTO
+import com.example.controlcomprasapp.data.local.dto.ProductoDTO
 import com.example.controlcomprasapp.data.repository.HomeRepository
 
 class HomeViewModel(private val repository: HomeRepository): ViewModel() {
     var items by mutableStateOf<List<DescuentosDTO>>(emptyList())
         private set
+    var items_gastos by mutableStateOf<List<ItemTicketDTO>>(emptyList())
+        private set
+    var items_prductos by mutableStateOf<List<ProductoDTO>>(emptyList())
+        private set
+    var items_mensual by mutableStateOf<List<GastoMensualDTO>>(emptyList())
+        private set
 
+    init {
+        loadAll()
+    }
+
+    private fun loadAll() {
+        loadDescuentosMax()
+        loadGastoXRubro()
+        loadGastosMensuales()
+        loadProdMasComprados()
+    }
     fun loadDescuentosMax(){
         items = repository.obtenerDescuentos()
+    }
+
+    fun loadGastoXRubro(){
+        items_gastos = repository.obtenerGastoXRubro()
+    }
+
+    fun loadProdMasComprados(){
+        items_prductos = repository.obtenerProdcutosMasComprados()
+    }
+
+    fun loadGastosMensuales(){
+        items_mensual = repository.obtenerGastoXMes()
     }
 }
