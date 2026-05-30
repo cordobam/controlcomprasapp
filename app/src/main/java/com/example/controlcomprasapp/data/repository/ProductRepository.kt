12bf598e04372e269541b461a1e8efe21fe39645
cordobam <cordobam1987@gmail.com>
2026-05-29 23:22:32 -0300

    Se hicieron cambios para que funcione correctamente en busqueda de prodcutos, el metodo en el repository y localdatasource para que se vean los locales cargados a mano en el dropdown

--- app/src/main/java/com/example/controlcomprasapp/data/repository/ProductRepository.kt
+++ app/src/main/java/com/example/controlcomprasapp/data/repository/ProductRepository.kt
@@ -1,16 +1,19 @@
 package com.example.controlcomprasapp.data.repository
 
 import com.example.controlcomprasapp.data.local.datasource.DescuentoDataSource
+import com.example.controlcomprasapp.data.local.datasource.LocalDataSource
 import com.example.controlcomprasapp.data.local.datasource.ProductFilter
 import com.example.controlcomprasapp.data.local.datasource.ProductoUI
 import com.example.controlcomprasapp.data.local.datasource.TicketLocalDataSource
 import com.example.controlcomprasapp.data.local.datasource.TipoProducto
 import com.example.controlcomprasapp.data.local.db.DbHelper
 import com.example.controlcomprasapp.domain.model.ItemTicket
+import com.example.controlcomprasapp.domain.model.Locales
 
 class ProductRepository(
     private val ticketLocalDataSource: TicketLocalDataSource,
-    private val descuentoDataSource: DescuentoDataSource
+    private val descuentoDataSource: DescuentoDataSource,
+    private val localesDataSource: LocalDataSource
 ) {
 
     fun obtenerItems(filter: ProductFilter): List<ProductoUI> {
@@ -29,6 +32,15 @@ class ProductRepository(
                 nombre = it.nombre,
                 total = it.total,
                 tipo = TipoProducto.DESCUENTO
+            )
+        }
+    }
+
+    fun obtenerLocales(): List<Locales> {
+        return localesDataSource.getAllLocales().map {
+            Locales(
+                id = it.id,
+                nombre = it.nombre
             )
         }
     }
